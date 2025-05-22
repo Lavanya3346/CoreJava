ab1dcc7e461ffdf6a7e61b2dc71a450a3750b081
Lavanya3346 <lavanyaeede2883@gmail.com>
2025-05-22 15:45:51 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+package lavanya;
+
+public class DataTypes {
+      public static void main(String [] args) {
+    	  System.out.println("She said,\"Java is fun!\"");
+    	  System.out.println();
+    	  System.out.println("------------Task2---------");
+    	  System.out.println("Java\tis\teasy\tto\tlearn");
+    	  System.out.println();
+    	  System.out.println("------------Task3---------");
+    	  System.out.println("\"Learn Java.\"\n\"Practice Java.\"\n\"Master Java.\"");
+      }
+}
