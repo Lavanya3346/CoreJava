4b55297ae031a353ecfcde13a095b4e147939f5f
Lavanya3346 <lavanyaeede2883@gmail.com>
2025-05-22 15:00:44 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,29 @@
+package lavanya;
+
+public class program {
+	public static void main(String[] args) {
+		System.out.println("My Family Details:");
+		System.out.println("Name: Koteswara Rao");
+		System.out.println("Relation: Father");
+		System.out.println("Age: 50");
+		System.out.println();
+		System.out.println("Name: Thirupathamma");
+		System.out.println("Relation: Mother");
+		System.out.println("Age: 45");
+		System.out.println();
+		System.out.println("Name: Mounika");
+		System.out.println("Relation: Sister");
+		System.out.println("Age: 24");
+		System.out.println();
+		System.out.println();
+		System.out.println("------------Task2-----------");
+		System.out.println("College Details:");
+		System.out.println("College Name: Andhra Loyola Institute of Engineering and Technology");
+		System.out.println("Course: B.Tech in Electronics and Communications Engineering");
+		System.out.println("Year: 4th Year");
+		System.out.println("Location: Vijayawada");
+		
+		
+	}
+
+}
