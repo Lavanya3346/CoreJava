03fa5d3349bd510a02873df1ece4c25b49e070c4
Lavanya3346 <lavanyaeede2883@gmail.com>
2025-05-22 16:20:20 +0530

    Add files via upload

---
+++ Primitivedatatypes.java
@@ -0,0 +1,25 @@
+package lavanya;
+
+public class Primitivedatatypes {
+	public static void main(String[]args){
+		byte b=10;
+		System.out.println("byte value ="+b);
+		short s=200;
+		System.out.println("short value:"+s);
+		int a=1000;
+		System.out.println("int value:"+a);
+		long l=100000L;
+		System.out.println("long value:"+l);
+		float p=10.5f;
+		System.out.println("float value:"+p);
+		double e=20.99;
+		System.out.println("double value:"+e);
+		char ch='A';
+		System.out.println("char value:"+ch);
+		boolean i=true;
+		System.out.println("boolean value:"+i);
+		String n ="Hello,Java";		
+		// TODO Auto-generated constructor stub
+	}
+
+}
