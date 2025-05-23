31a1514a3b2a62a3a39a232237d4254737ae89ed
Lavanya3346 <lavanyaeede2883@gmail.com>
2025-05-23 22:17:23 +0530

    Add files via upload

---
+++ ArthmeticOperators.java
@@ -0,0 +1,27 @@
+package lavanya;
+
+public class ArthmeticOperators {
+public static void main(String[] args) {
+	int a = 16;
+	int b = 7;
+	System.out.println("Arthmetic Operators");
+	System.out.println("Add:"+ (a+b));
+	System.out.println("Sub:"+(a-b));
+	System.out.println("mul:"+(a*b));
+	System.out.println("div:"+(a/b));
+	System.out.println("moduls:"+(a%b));
+	//Relational Operators
+	System.out.println("Relational Operators");
+	System.out.println("lessthan:"+(a<b));
+	System.out.println("graterthan:"+(a>b));
+	System.out.println("equalto:"+(a==b));
+	System.out.println("not equalto:"+(a!=b));
+	System.out.println("gteaterthan equal:"+(a<=b));
+	System.out.println("lessthan equal:"+(a>=b));
+	
+	
+	
+	
+	
+}
+}
