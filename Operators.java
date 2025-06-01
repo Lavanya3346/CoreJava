51151d008808734070b920b8ff4a0de5c04d972f
Lavanya3346 <lavanyaeede2883@gmail.com>
2025-06-01 21:19:10 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,45 @@
+package lavanya;
+
+public class Operators {
+	public class Operatorss {
+	    public static void add()
+	    {
+	    	int a=5;
+	    	  int  b=10;
+	    	System.out.println("addition:"+(a+b));
+	    }
+	    public static void sub()
+	    {
+	    	int a=5;
+	    	  int  b=10;
+	    	System.out.println("subration:"+(a-b));
+	    }
+	    public static void mul()
+	    {
+	    	int a=5;
+	    	  int  b=10;
+	    	System.out.println("multiplcation:"+(a*b));
+	    }
+	    public static void div()
+	    {
+	    	int a=5;
+	    	  int  b=10;
+	    	System.out.println("divide:"+(a/b));
+	    }
+	    public static void rem()
+	    {
+	    	int a=5;
+	    	  int  b=10;
+	    	System.out.println("remainder:"+(a%b));
+	    }
+	 public static void main(String[] args) {
+		add();
+		sub();
+		mul();
+		div();
+		rem();
+		}
+
+	}
+
+}
